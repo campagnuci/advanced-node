3bec644d3cec005262af08486ff7d26a984962f3
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-15 20:19:08 -0300

    feat: ensure Service calls LoadUserAccountRepository when LoadFacebookUserApi returns data

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,4 +1,5 @@
 import { FacebookAuthenticationService } from '@/data/services/facebook-authentication'
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { mock, MockProxy } from 'jest-mock-extended'
@@ -6,12 +7,19 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('Facebook Authentication Service', () => {
   let sut: FacebookAuthenticationService
   let loadFacebookUserApiSpy: MockProxy<LoadFacebookUserApi>
+  let loadUserAccountRepo: MockProxy<LoadUserAccountRepository>
 
   const token: string = 'any_token'
 
   beforeEach(() => {
     loadFacebookUserApiSpy = mock()
-    sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+    loadFacebookUserApiSpy.loadUser.mockResolvedValue({
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+    loadUserAccountRepo = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApiSpy, loadUserAccountRepo)
   })
 
   test('should call LoadFacebookUserApi with correct params', async () => {
@@ -24,5 +32,11 @@ describe('Facebook Authentication Service', () => {
     loadFacebookUserApiSpy.loadUser.mockResolvedValueOnce(undefined)
     const authResult = await sut.perform({ token })
     expect(authResult).toEqual(new AuthenticationError())
+  })
+
+  test('should call LoadUserAccountRepo when LoadFacebookUserApi returns data', async () => {
+    await sut.perform({ token })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email: 'any_fb_email' })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1)
   })
 })
