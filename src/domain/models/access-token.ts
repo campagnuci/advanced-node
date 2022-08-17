f255161df54cfbbc972aacb8dc791a2b6c882e6c
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-16 21:02:09 -0300

    feat: ensure access token expires in 30minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,9 @@
-export type AccessToken = {
-  accessToken: string
+const THIRTY_MINUTES_IN_MILLISECONDS = 1800000
+
+export class AccessToken {
+  constructor (private readonly value: string) {}
+
+  static get expirationInMs (): number {
+    return THIRTY_MINUTES_IN_MILLISECONDS
+  }
 }
