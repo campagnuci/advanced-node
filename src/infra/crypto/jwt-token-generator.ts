7b85394bc6568cc56908546f67a0cf3ba1324e7c
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-17 21:11:10 -0300

    feat: ensure JwtTokenGenerator returns token on success

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -1,12 +1,12 @@
 import { TokenGenerator } from '@/data/contracts/crypto'
 import jwt from 'jsonwebtoken'
 
-export class JwtTokenGenerator {
+export class JwtTokenGenerator implements TokenGenerator {
   constructor (private readonly secret: string) {}
 
   async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000
-    await jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
-    return ''
+    const token = await jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
+    return token
   }
 }
--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -4,10 +4,25 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken')
 
 describe('JwtTokenGenerator', () => {
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+    fakeJwt.sign.mockImplementation(() => 'any_token')
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret')
+  })
+
   test('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+  })
+
+  test('should return a token', async () => {
+    const token = await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    expect(token).toBe('any_token')
   })
 })
