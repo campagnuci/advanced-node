8f36a4c277b7e0f021f6b9ba6cfc2f3328820144
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-19 17:32:32 -0300

    feat: add ValidationComposite to handle multiple validations

---
+++ tests/application/validation/validation-composite.spec.ts
@@ -0,0 +1,39 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+import { ValidationComposite, Validator } from '@/application/validation'
+
+describe('ValidationComposite', () => {
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
+
+  beforeAll(() => {
+    validator1 = mock<Validator>()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock<Validator>()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  test('should return undefined if all validators return undefined', () => {
+    const error = sut.validate()
+    expect(error).toBeUndefined()
+  })
+
+  test('should return the first error', () => {
+    validator1.validate.mockReturnValueOnce(new Error('error_1'))
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
+    const error = sut.validate()
+    expect(error).toEqual(new Error('error_1'))
+  })
+
+  test('should return the error', () => {
+    validator2.validate.mockReturnValueOnce(new Error('error_2'))
+    const error = sut.validate()
+    expect(error).toEqual(new Error('error_2'))
+  })
+})
