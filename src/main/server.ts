4d66add421db18057680b6bde7545d6d6bd4ce8c
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-20 22:43:34 -0300

    refactor: remove ormconfig from infra layer to a config file on project structure

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,8 +3,7 @@ import 'reflect-metadata'
 import { app } from './config/app'
 import { env } from './config/env'
 import { createConnection } from 'typeorm'
-import { config } from '@/infra/postgres/helpers'
 
-createConnection(config)
+createConnection()
   .then(() => app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)))
   .catch(console.error)
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -12,7 +12,7 @@ describe('Facebook API Integration Tests', () => {
   })
 
   test('should return a Facebook User if token is valid', async () => {
-    const user = await sut.loadUser({ token: 'EAAIdxjHwhIkBAGeubz80tDwZAmQeX1ANJYLgP58ZAcMfWZCxqWZBlALlXXxoXmGFiwWpYh1ZB16m9DUTjLVSEtpZAlNZBmmecZAZBRDrtWULpJvoIH2UepDGyIBytWtLbgXRH0Bn8Sk05cIZCirZBUqK7ZAdBNs5EsPI5ZBD9TkZBiYvwkfmH1DZBDRf6oGkZAm6yZBcrzC4JWulZCbR6nuQSN2krabZBGs' })
+    const user = await sut.loadUser({ token: 'EAAIdxjHwhIkBAEa3dZACZBKecQ6ZAZCrkD6qZB0qZC7cZA4TT7bF7XYnok26d1WnBpZBSBZCjaNA0ZCypWT6BKiYKJ8U9xuXJJmAUbzZCOPbrZBwsuqZBAJZCUfa5n50bZCfIjDY4cMzBoLLpUogIEZAxTJJCZBZADHfxPdpxumK0xI30A7TWlKa4rvkE4RCQHfrTkZCDZBIHYs4vDe9yFsb0QG5OowwW1FG' })
     expect(user).toEqual({
       facebookId: '110474708445035',
       email: 'vinnicius_zzmxkiq_teste@tfbnw.net',
