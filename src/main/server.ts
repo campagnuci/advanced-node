d55caac6c5ff95720100017476685bd94cff9cf6
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-20 22:23:21 -0300

    feat: connect to postgres db before server init

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,9 @@ import './config/module-alias'
 import 'reflect-metadata'
 import { app } from './config/app'
 import { env } from './config/env'
+import { createConnection } from 'typeorm'
+import { config } from '@/infra/postgres/helpers'
 
-app.listen(env.port, () => {
-  console.log(`Server running at http://localhost:${env.port}`)
-})
+createConnection(config)
+  .then(() => app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`)))
+  .catch(console.error)
