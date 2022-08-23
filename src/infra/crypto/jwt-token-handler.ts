2f9052a9a024b0ea169cce8b14ddd94103ffbb0b
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-08-23 20:05:11 -0300

    feat: ensure JwtTokenHandler calls verify with correct params

--- src/infra/crypto/jwt-token-handler.ts
+++ src/infra/crypto/jwt-token-handler.ts
@@ -1,15 +1,17 @@
-import { TokenGenerator } from '@/domain/contracts/crypto'
-import jwt from 'jsonwebtoken'
-
-type Params = TokenGenerator.Params
-type Result = TokenGenerator.Result
+import { TokenGenerator, TokenValidator } from '@/domain/contracts/crypto'
+import { sign, verify } from 'jsonwebtoken'
 
 export class JwtTokenHandler implements TokenGenerator {
   constructor (private readonly secret: string) {}
 
-  async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
+  async generateToken ({ expirationInMs, key }: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = expirationInMs / 1000
-    const token = await jwt.sign({ key }, this.secret, { expiresIn: expirationInSeconds })
+    const token = await sign({ key }, this.secret, { expiresIn: expirationInSeconds })
     return token
+  }
+
+  async validateToken ({ token }: TokenValidator.Params): Promise<TokenValidator.Result> {
+    verify(token, this.secret)
+    return ''
   }
 }
--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -33,6 +33,7 @@ describe('JwtTokenHandler', () => {
     test('should call sign with correct params', async () => {
       await sut.generateToken({ key, expirationInMs })
       expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
     })
 
     test('should return a token', async () => {
@@ -44,6 +45,21 @@ describe('JwtTokenHandler', () => {
       fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
       const promise = sut.generateToken({ key, expirationInMs })
       await expect(promise).rejects.toThrow(new Error('token_error'))
+    })
+  })
+
+  describe('validateToken', () => {
+    let token: string
+
+    beforeAll(() => {
+      token = 'any_token'
+      // fakeJwt = jwt as jest.Mocked<typeof jwt>
+    })
+
+    test('should call sign with correct params', async () => {
+      await sut.validateToken({ token })
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
     })
   })
 })
