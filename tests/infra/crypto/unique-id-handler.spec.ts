514b92b3dad5c8f94955f1e7dfb9bada5475b83b
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-09-01 19:12:23 -0300

    feat: create UniqueId implementation as to not use a dependency

---
+++ tests/infra/crypto/unique-id-handler.spec.ts
@@ -0,0 +1,17 @@
+import { UniqueId } from '@/infra/crypto'
+
+describe('UniqueId Handler', () => {
+  let sut: UniqueId
+
+  test('should call uuid.v4 correctly', () => {
+    sut = new UniqueId(new Date(2022, 8, 1, 10, 10, 10))
+    const uuid = sut.uuid({ key: 'any_key' })
+    expect(uuid).toBe('any_key_20220901101010')
+  })
+
+  test('should call uuid.v4 correctly', () => {
+    sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0))
+    const uuid = sut.uuid({ key: 'any_key' })
+    expect(uuid).toBe('any_key_20180310180100')
+  })
+})
