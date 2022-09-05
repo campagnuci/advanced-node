9295612b6a33d6511d5802c2e38d2e30d6c013a2
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-09-05 19:18:08 -0300

    refactor: move AwsS3FileStorage to source dir

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,29 +1,7 @@
-import { UploadFile } from '@/domain/contracts/gateways'
+import { AwsS3FileStorage } from '@/infra/gateways'
 import { config, S3 } from 'aws-sdk'
 
 jest.mock('aws-sdk')
-
-export class AwsS3FileStorage {
-  constructor (accessKey: string, secret: string, private readonly bucket: string) {
-    config.update({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secret
-      }
-    })
-  }
-
-  async upload (input: UploadFile.Input): Promise<UploadFile.Output> {
-    const s3 = new S3()
-    await s3.putObject({
-      Bucket: this.bucket,
-      Key: input.key,
-      Body: input.file,
-      ACL: 'public-read'
-    }).promise()
-    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(input.key)}`
-  }
-}
 
 describe('AwsS3FileStorage', () => {
   let sut: AwsS3FileStorage
