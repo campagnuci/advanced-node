a72b04b3909c1532ea7d6fa3b8cc45a802eb920d
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2022-09-07 19:39:53 -0300

    feat: add DeleteProfilePicture Controller

---
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -0,0 +1,32 @@
+import { Controller, DeletePictureController } from '@/application/controllers'
+
+describe('DeletePictureController', () => {
+  let sut: DeletePictureController
+  let changeProfilePicture: jest.Mock
+
+  beforeAll(() => {
+    changeProfilePicture = jest.fn()
+  })
+
+  beforeEach(() => {
+    sut = new DeletePictureController(changeProfilePicture)
+  })
+
+  test('should extend Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+
+  test('should call ChangeProfilePicture with correct input', async () => {
+    await sut.handle({ userId: 'any_user_id' })
+    expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id', file: undefined })
+    expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return 204', async () => {
+    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
+  })
+})
